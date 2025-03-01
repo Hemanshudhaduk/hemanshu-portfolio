1ba6ce7aa0f3f7b7a7f74f5cd86e68b1cc3c45af
Hemanshudhaduk <115709310+Hemanshudhaduk@users.noreply.github.com>
2025-03-01 15:51:24 +0530

    Fixed deployment issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/hemanshu-portfolio/', // 👈 Required for GitHub Pages
+  build: {
+    outDir: 'dist',
+  }
+});
